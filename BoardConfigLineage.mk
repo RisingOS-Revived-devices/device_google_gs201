4eb86c9cd1d95339324442218a576e7256929d04
sidharthify <wednisegit@gmail.com>
2025-06-21 21:50:14 +0000

    gs201: RisingOS Bringup
    Signed-off-by: sidharthify <wednisegit@gmail.com>

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -5,7 +5,7 @@
 #
 
 # Manifests
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/yaap/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += device/google/gs201/configs/manifests/android.hardware.security.rkp-service.citadel.xml
 DEVICE_MANIFEST_FILE += device/google/gs201/configs/manifests/manifest_radio_ds.xml
 
