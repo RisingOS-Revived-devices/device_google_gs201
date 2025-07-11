b8dc4fcb2708c41618161e2a545d0963e25266f7
Austin Young <austineyoung2000@gmail.com>
2025-07-11 16:04:58 -0400

    gs201: Allow running 32-bit apps
    Change-Id: Ibf1e2d8313b0a2fd09bb0cf4c17f2774f20ee15a

--- device.mk
+++ device.mk
@@ -866,8 +866,11 @@ include device/google/gs-common/gps/brcm/device.mk
 endif
 
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
-
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+#$(call inherit-product, hardware/google_devices/exynos5/exynos5.mk)
+#$(call inherit-product-if-exists, hardware/google_devices/gs201/gs201.mk)
+#$(call inherit-product-if-exists, vendor/google_devices/common/exynos-vendor.mk)
+#$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4375/device-bcm.mk)
 include device/google/gs-common/sensors/sensors.mk
 $(call soong_config_set,usf,target_soc,gs201)
 
